3daf18e4f863562222e4b50fcd90320928835cd4
rj9889 <rajjain.bit@gmail.com>
2026-01-26 22:07:00 -0500

    Create app.jsx

---
+++ aiapp/app.jsx
@@ -0,0 +1,19 @@
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+#root {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, Helvetica, Arial, sans-serif;
+  background-color: #f3f4f6;
+  color: #111827;
+}
